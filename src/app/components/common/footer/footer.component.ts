32474855b5e313b69446ee34890b831fea7cc78a
luis <luisginza10@avis.com>
2020-09-28 10:40:07 -0400

    update social media

--- src/app/components/common/footer/footer.component.ts
+++ src/app/components/common/footer/footer.component.ts
@@ -22,11 +22,11 @@ export class FooterComponent implements OnInit {
       this.loadServ.openDialog();
       const email = contactForm.value;
       const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-      this.http.post('https://formspree.io/myynqazj',
+      this.http.post('https://formspree.io/xeqpogak',
         { name: email.name, replyto: email.email, message: email.messages },
         { 'headers': headers }).subscribe(
           response => {
-            this.notiserv.success('Gracias por comunicase con nosotros.!');
+            this.notiserv.success('Gracias por comunicarse con nosotros.!');
             contactForm.reset();
             this.loadServ.close();
           }
