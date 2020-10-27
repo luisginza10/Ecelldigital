3a86972b50f4d8a88b439d45f6fc7a434e3a4212
luis <luisginza10@avis.com>
2020-10-27 14:42:55 -0300

    update api url aws

--- src/app/shared/baseurl.service.ts
+++ src/app/shared/baseurl.service.ts
@@ -5,11 +5,12 @@ import { Injectable } from '@angular/core';
 })
 export class BaseurlService {
   /*
-    cloud: https://aviscelldigital.herokuapp.com/
+    cloud 1: https://aviscelldigital.herokuapp.com/
+    cloud 2 aws: http://awscelldigitalapp-env.eba-kavzbny8.us-east-2.elasticbeanstalk.com/
     desarrollo:return 'http://localhost:8080/';
   */
   constructor() { }
   getBaseUrl(): string {
-    return 'https://aviscelldigital.herokuapp.com/';
+    return 'http://awscelldigitalapp-env.eba-kavzbny8.us-east-2.elasticbeanstalk.com/';
   }
 }
